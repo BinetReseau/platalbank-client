4049434016e85773f2240bebd8cb70ceeada123e
Varal7 <varaal@gmail.com>
2015-11-08 22:20:18 +0100

    Fetch transaction list from API

--- src/app/components/transactionlist/transactionlist.controller.js
+++ src/app/components/transactionlist/transactionlist.controller.js
@@ -3,16 +3,9 @@
 angular
 .module('platalbankClient')
 .controller('TransactionlistController', ['$scope','$http',function($scope,$http){
-    $scope.transactions = [{
-        "id": 1,
-        "state": "P",
-        "amount": 25,
-        "label": "Nom d'une transaction",
-        "debited_account": "http://127.0.0.1:8000/api/account/1/",
-        "credited_account": "http://127.0.0.1:8000/api/account/2/",
-        "timestamp": "2015-10-18T14:11:15.102151Z",
-        "last_modified": "2015-10-18T14:11:06.911348Z",
-        "author": "Dummy"
-    }];
+    $http.get('http://localhost:8000/api/transaction/')
+        .then(function(res){
+            $scope.transactions = res.data.results;   
+        });
 }]);
 
