52cc90718ef78fe2eaa5194444ecfbf6426f82b7
Snoopy <anthonyhtyao@gmail.com>
2015-12-22 02:05:45 +0100

    Add transactions' page Group transavtions' list by date Bonnes vacances

--- src/app/components/transactionlist/transactionlist.controller.js
+++ src/app/components/transactionlist/transactionlist.controller.js
@@ -2,47 +2,46 @@
 
 angular
 .module('platalbankClient')
-.controller('TransactionlistController', ['$scope','$http',function($scope,$http){
+.controller('TransactionlistController', ['$scope','$http','$q',function($scope,$http,$q){
+    function fetchTransactionList(){
+        $http.get('http://localhost:8000/api/transaction/').then(function(res){
+            $scope.transactions = res.data.results;
+            var len = $scope.transactions.length;
+            for(var i=0;i < len;i++){
+                $scope.transactions[i].created_date = timeTransfer($scope.transactions[i].created)[0];
+                $scope.transactions[i].created_time = timeTransfer($scope.transactions[i].created)[1];
+                $scope.transactions[i].last_modified_date = timeTransfer($scope.transactions[i].last_modified)[0];
+                $scope.transactions[i].last_modified_time = timeTransfer($scope.transactions[i].last_modified)[1];
 
-    $http.get('http://localhost:8000/api/event/')
-        .then(function(res){
-            var l = res.data.count;
-            $scope.events = new Array(l);
-            $scope.transactions = new Array(l);
-            $scope.transactionss = new Array(l);
-            for(var i=0;i<l;i++){
-                var x = res.data.results[i];
-                $scope.events[x.id-1] = x.label;
-                $scope.transactions[i] = [];
-                $scope.transactionss[i] = [];
+            };
+            groupByTime();
+        })
+    };
+
+    function timeTransfer(str){
+        var lst = str.split("T");
+        lst[1] = lst[1].split(".")[0];
+        return lst;
+    };
+
+    function groupByTime(){
+        var len = $scope.transactions.length;
+        var timeUsed = [];
+        $scope.groupBy = [];
+        for(var i=0;i < len; i++){
+            var currentDate = $scope.transactions[i].last_modified_date;
+            var ind = timeUsed.indexOf(currentDate);
+            if(ind != -1){
+                $scope.groupBy[ind].transactions.push($scope.transactions[i]);
             }
-    })
-    $http.get('http://localhost:8000/api/transaction/')
-        .then(function(res){
-            var l =res.data.count;
-            for (var i=0;i<l;i++){
-                var x = res.data.results[i];
-                var url = x.event;
-                var ind = parseInt(url[url.length-2]);
-                $scope.transactions[ind-1].push(x);
-            };
-        });
-    //Pour tester
-    /*$http.get('http://localhost:8000/api/transaction/')
-        .then(function(res){
-            var l =res.data.count;
-            for (var i=0;i<l;i++){
-                var x = res.data.results[i];
-                $scope.transactionss[i] = x;
-                var url = x.event;
-                $http.get(url)
-                    .then(function(res){
-                        $scope.transactionss[i].event=res.data;
-                        //console.log($scope.transactionss[i]);
-                    });
-            };
+            else{
+                $scope.groupBy.push({date:currentDate,transactions:[]});
+                timeUsed.push(currentDate);
+                ind = timeUsed.indexOf(currentDate);
+                $scope.groupBy[ind].transactions.push($scope.transactions[i]);
+            }
+        }
+    }
 
-        });
-    */
+    fetchTransactionList();
 }]);
-
